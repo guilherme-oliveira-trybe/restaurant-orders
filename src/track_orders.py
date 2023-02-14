fbbcc0756df24223485df416d28a2627085f9c9d
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2023-02-13 21:00:05 -0300

    feat: class TrackOrders controle continuo das informações de marketing

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,22 +1,44 @@
+from analyze_log import (
+    most_order_plate_by_client,
+    plates_never_ask_by_client,
+    days_never_went_to_restaurant,
+)
+
+
 class TrackOrders:
     # aqui deve expor a quantidade de estoque
+    def __init__(self) -> None:
+        self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, customer, order, day):
-        pass
+        self.orders.append({"cliente": customer, "pedido": order, "dia": day})
 
     def get_most_ordered_dish_per_customer(self, customer):
-        pass
+        return most_order_plate_by_client(customer, self.orders)
 
     def get_never_ordered_per_customer(self, customer):
-        pass
+        return plates_never_ask_by_client(customer, self.orders)
 
     def get_days_never_visited_per_customer(self, customer):
-        pass
+        return days_never_went_to_restaurant(customer, self.orders)
 
     def get_busiest_day(self):
-        pass
+        busiest_day = {}
+        for order in self.orders:
+            if order["dia"] not in busiest_day:
+                busiest_day[order["dia"]] = 0
+            else:
+                busiest_day[order["dia"]] += 1
+        return str(max(busiest_day, key=busiest_day.get))
 
     def get_least_busy_day(self):
-        pass
+        least_busy_day = {}
+        for order in self.orders:
+            if order["dia"] not in least_busy_day:
+                least_busy_day[order["dia"]] = 0
+            else:
+                least_busy_day[order["dia"]] += 1
+        return str(min(least_busy_day, key=least_busy_day.get))
